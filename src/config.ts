82f5c77706522f7090a33fc60f8fbeb1741e36c4
YanDeLeon <juliandeleon@Julians-MBP.localdomain>
2023-01-20 11:36:46 -0500

    adding tailwind and react

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Julian De Leon';
+export const SITE_TITLE = 'Julian De Leon | Software Engineer';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
